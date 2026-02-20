92b1115857d5a89dac4e7fb9e0a33bc06c2e4b5b
User <user@example.com>
2026-02-20 10:31:23 +0530

    .\script.js

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
-console.log("Hello world");
-document.write("Hello world");
+// console.log("Hello world");
+// document.write("Hello world");
+
+// var num = 10;
+// console.log(num);
+// console.log(typeof(num));
+// num=false;
+// console.log(num);
+// console.log(typeof (num));
+// num ="swarit sahu";
+// console.log(num);
+// console.log(typeof (num));
+// mun=null;
+// console.log(mun);
+// console.log(typeof(num));
+// num =undefined;
+// console.log(num);
+// console.log(typeof(num));
+    // const sym1 =symble(4);
+    // console.log(sym1);
+    // const sym2 =symble(4);
+    // console.log(sym2);
+    // if(sym1===sym2){
+    //     console.log("Both are same");
+    // }else{
+    //     console.log("Both are not same")
+    // }
+    var a =100; var b=13; var c=10; var linebreak="<br>";
+    document.write("a+b+c=");
+    result = a+b+c;
+    document.write(result);
+    document.write(linebreak);
+    document.write("a-b-c=");
+    result = a-b-c;
+    document.write(result);
+    document.write(linebreak);
